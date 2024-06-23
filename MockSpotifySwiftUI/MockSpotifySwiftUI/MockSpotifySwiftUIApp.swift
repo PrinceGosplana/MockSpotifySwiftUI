ee1bb2db214449488c2c2c4de1ab0251881d0a6c
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-23 10:07:41 +0300

    init project

---
+++ MockSpotifySwiftUI/MockSpotifySwiftUI/MockSpotifySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockSpotifySwiftUIApp.swift
+//  MockSpotifySwiftUI
+//
+//  Created by Oleksandr Isaiev on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MockSpotifySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
