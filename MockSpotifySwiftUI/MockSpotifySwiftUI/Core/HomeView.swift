2eb2e0a95ee7b3ae6fdd0aa3e99d6206d9b7bcf7
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-23 15:20:55 +0300

    add simple mock scrollview on home screen

--- MockSpotifySwiftUI/MockSpotifySwiftUI/Core/HomeView.swift
+++ MockSpotifySwiftUI/MockSpotifySwiftUI/Core/HomeView.swift
@@ -14,13 +14,25 @@ struct HomeView: View {
             Color.spotifyBlack.ignoresSafeArea()
 
             HStack {
-                Image(.theGreatWave)
+                Image(User.mockUser.image)
+                    .resizable()
                     .frame(width: 30, height: 30)
                     .background(.spotifyWhite)
                     .clipShape(Circle())
                     .onTapGesture {
 
                     }
+
+                ScrollView(.horizontal) {
+                    HStack(spacing: 8) {
+                        ForEach(0..<20) { _ in
+                            Rectangle()
+                                .fill(Color.mint)
+                                .frame(width: 20, height: 20)
+                        }
+                    }
+                }
+                .scrollIndicators(.hidden)
             }
         }
     }
