9df3bdc8561fdd22ae893053a4e122cec13e678a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-28 21:37:42 +0300

    create empty screen for playlist

---
+++ MockSpotifySwiftUI/MockSpotifySwiftUI/Core/PlaylistView.swift
@@ -0,0 +1,22 @@
+//
+//  PlaylistView.swift
+//  MockSpotifySwiftUI
+//
+//  Created by Oleksandr Isaiev on 28.06.2024.
+//
+
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        ZStack {
+            Color.spotifyBlack.ignoresSafeArea()
+
+            
+        }
+    }
+}
+
+#Preview {
+    PlaylistView()
+}
